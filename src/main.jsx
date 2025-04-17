0c22f2d61ce8b884f973f745350eef433af2111d
Anudeep Debbata <30561588+Anudepp@users.noreply.github.com>
2025-04-17 15:22:53 +0530

    folder structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,13 @@
-// src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
 import App from "./App";
+import { Provider } from "react-redux";
 import store from "./store";
-import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <App />
     </Provider>
   </React.StrictMode>
 );
